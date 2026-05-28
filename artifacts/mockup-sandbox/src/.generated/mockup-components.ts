e3c0003bcbc9fa63c3795028abe8bf055f2154f8
hgautier <58014399-hgautier@users.noreply.replit.com>
2026-05-28 00:28:25 +0000

    Git commit prior to merge

--- artifacts/mockup-sandbox/src/.generated/mockup-components.ts
+++ artifacts/mockup-sandbox/src/.generated/mockup-components.ts
@@ -2,6 +2,13 @@
 type ModuleMap = Record<string, () => Promise<Record<string, unknown>>>;
 export const modules: ModuleMap = {
   "./components/mockups/customer-card-wireframes/CustomerCardWireframes.tsx": () => import("../components/mockups/customer-card-wireframes/CustomerCardWireframes.tsx"),
+  "./components/mockups/customers-list/AccessibilityReadability.tsx": () => import("../components/mockups/customers-list/AccessibilityReadability.tsx"),
+  "./components/mockups/customers-list/AffordanceVisibility.tsx": () => import("../components/mockups/customers-list/AffordanceVisibility.tsx"),
+  "./components/mockups/customers-list/HierarchyClarity.tsx": () => import("../components/mockups/customers-list/HierarchyClarity.tsx"),
+  "./components/mockups/header-variants/CommandPalette.tsx": () => import("../components/mockups/header-variants/CommandPalette.tsx"),
+  "./components/mockups/header-variants/ContextualStrip.tsx": () => import("../components/mockups/header-variants/ContextualStrip.tsx"),
+  "./components/mockups/header-variants/FloatingCard.tsx": () => import("../components/mockups/header-variants/FloatingCard.tsx"),
+  "./components/mockups/header-variants/SearchForward.tsx": () => import("../components/mockups/header-variants/SearchForward.tsx"),
   "./components/mockups/home-variants/CommandDashboard.tsx": () => import("../components/mockups/home-variants/CommandDashboard.tsx"),
   "./components/mockups/home-variants/NextActionFocus.tsx": () => import("../components/mockups/home-variants/NextActionFocus.tsx"),
   "./components/mockups/home-variants/PolishedSections.tsx": () => import("../components/mockups/home-variants/PolishedSections.tsx"),
@@ -13,10 +20,6 @@ export const modules: ModuleMap = {
   "./components/mockups/sales-cards/VariantB1.tsx": () => import("../components/mockups/sales-cards/VariantB1.tsx"),
   "./components/mockups/sales-cards/VariantB2.tsx": () => import("../components/mockups/sales-cards/VariantB2.tsx"),
   "./components/mockups/sales-cards/VariantC.tsx": () => import("../components/mockups/sales-cards/VariantC.tsx"),
-  "./components/mockups/header-variants/CommandPalette.tsx": () => import("../components/mockups/header-variants/CommandPalette.tsx"),
-  "./components/mockups/header-variants/ContextualStrip.tsx": () => import("../components/mockups/header-variants/ContextualStrip.tsx"),
-  "./components/mockups/header-variants/FloatingCard.tsx": () => import("../components/mockups/header-variants/FloatingCard.tsx"),
-  "./components/mockups/header-variants/SearchForward.tsx": () => import("../components/mockups/header-variants/SearchForward.tsx"),
   "./components/mockups/signin-approaches/AccessibilityReadability.tsx": () => import("../components/mockups/signin-approaches/AccessibilityReadability.tsx"),
   "./components/mockups/signin-approaches/AffordanceVisibility.tsx": () => import("../components/mockups/signin-approaches/AffordanceVisibility.tsx"),
   "./components/mockups/signin-approaches/Conversational.tsx": () => import("../components/mockups/signin-approaches/Conversational.tsx"),
@@ -30,8 +33,5 @@ export const modules: ModuleMap = {
   "./components/mockups/trades-card/InteractionAffordance.tsx": () => import("../components/mockups/trades-card/InteractionAffordance.tsx"),
   "./components/mockups/trades-card/KanbanByTrade.tsx": () => import("../components/mockups/trades-card/KanbanByTrade.tsx"),
   "./components/mockups/trades-card/QuickDial.tsx": () => import("../components/mockups/trades-card/QuickDial.tsx"),
-  "./components/mockups/trades-card/ScannableList.tsx": () => import("../components/mockups/trades-card/ScannableList.tsx"),
-  "./components/mockups/customers-list/AccessibilityReadability.tsx": () => import("../components/mockups/customers-list/AccessibilityReadability.tsx"),
-  "./components/mockups/customers-list/AffordanceVisibility.tsx": () => import("../components/mockups/customers-list/AffordanceVisibility.tsx"),
-  "./components/mockups/customers-list/HierarchyClarity.tsx": () => import("../components/mockups/customers-list/HierarchyClarity.tsx")
+  "./components/mockups/trades-card/ScannableList.tsx": () => import("../components/mockups/trades-card/ScannableList.tsx")
 };
